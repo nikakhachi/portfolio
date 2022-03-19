20feb792cb3282ba9e3f30eb3fb4aebb8320f817
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2022-03-19 17:02:44 +0400

    add deemo three js background

---
+++ src/background/index.jsx
@@ -0,0 +1,61 @@
+import * as THREE from "three";
+import { OrbitControls } from "@three-ts/orbit-controls";
+
+import styles from "./styles.module.css";
+import { useEffect } from "react";
+
+const Background = () => {
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGL1Renderer({ canvas: document.querySelector("#bg"), alpha: true });
+
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.position.setZ(40);
+
+    document.body.appendChild(renderer.domElement);
+
+    const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff, wireframe: true });
+    const torus = new THREE.Mesh(geometry, material);
+
+    scene.add(torus);
+
+    const pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.set(0, 0, 0);
+    const ambientLight = new THREE.AmbientLight(0xffffff);
+    scene.add(pointLight, ambientLight);
+
+    const spaceTexture = new THREE.TextureLoader().load("/4k.jpg");
+    scene.background = spaceTexture;
+    scene.background.encoding = THREE.sRGBEncoding;
+
+    const lightHelper = new THREE.PointLightHelper(pointLight);
+    const gridHelper = new THREE.GridHelper(200, 50);
+    scene.add(gridHelper, lightHelper);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    function animate() {
+      requestAnimationFrame(animate);
+      torus.rotation.x += 0.01;
+      torus.rotation.y += 0.005;
+      torus.rotation.z += 0.01;
+      controls.update();
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.render(scene, camera);
+    }
+
+    animate();
+  }, []);
+
+  return (
+    <>
+      <canvas className={styles.bgCanvas} id="bg"></canvas>
+    </>
+  );
+};
+
+export default Background;
