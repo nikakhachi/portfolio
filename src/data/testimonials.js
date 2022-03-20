2512ff379d3ae688323004c0097bea6014208328
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2022-03-20 21:15:10 +0400

    add testimonial by : Lado Demuradze

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -9,23 +9,26 @@ const testimonials = [
   //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
   // },
   {
+    author: "Lado Demuradze",
+    photo: "assets/testimonials/profile3.jpeg",
+    company: "Pulsar AI Georgia",
+    companyUrl: "https://www.linkedin.com/company/pulsar-ai-georgia/",
+    position: "Project Manager",
+    testimonial: `Nika is a very hard-working and detail-oriented person, always doing his best to resolve 
+      all tasks which were planned at Sprint Planning. If his teammates have some problems with their
+      tasks, Nika is always ready to help. He is excited about his profession and this is an 
+      extra point for him to be a self-motivated and a professional developer.`,
+  },
+  {
     author: "David Maghradze",
     photo: "assets/testimonials/profile2.jpg",
     company: "Aidnet",
     companyUrl: "https://www.linkedin.com/company/aidnet/",
     position: "Co-Founder, CIO",
-    testimonial:
-      "As a React Developer, Nika did really bring the value which helped the company to gain wealth and move forwards. He would be a tremendous asset to your company and has my highest recommendation.",
+    testimonial: `As a React Developer, Nika did really bring the value which helped the company to 
+      gain wealth and move forwards. He would be a tremendous asset to your company and 
+      has my highest recommendation.`,
   },
-  // {
-  //   author: "Lado Demuradze",
-  //   photo: "assets/testimonials/profile3.jpeg",
-  //   company: "Supernova",
-  //   companyUrl: "https://en.supernova.ge",
-  //   position: "Project Manager",
-  //   testimonial:
-  //     "Lorem ipsued do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-  // },
 ];
 
 export default testimonials;
